0fc58f28b9e7a5b3b0999354593133a7b3e6abe3
szymonw <handerdog3@gmail.com>
2025-10-14 23:01:39 +0200

    example edited

--- install/example.cpp
+++ install/example.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	NormalEquation a;
 	a.fit(arma::mat({1,2,3}).t(), arma::vec({2,4,6}));
-	std::cout << a.predict(arma::mat({812}));
+	double a = 4;
+	std::cout << "Predicted value for " << a << " is " << a.predict(arma::mat({a})) << "\n";
 	return 0;
 }
