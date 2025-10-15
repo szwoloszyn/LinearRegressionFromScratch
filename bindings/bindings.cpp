f1bd6542ffeef2299d0e77fbdd4b662bdffa94de
szymonw <handerdog3@gmail.com>
2025-10-16 01:29:47 +0200

    core bindings file implemented

--- bindings/bindings.cpp
+++ bindings/bindings.cpp
@@ -7,18 +7,28 @@
 
 namespace py = pybind11;
 
+// PYBIND11_MODULE(linregpy, m) {
+//     m.doc() = "Python bindings for the C++ LinearRegression library";
+//     py::class_<LinearRegression>(m, "LinearRegression")
+//         .def("fit", &LinearRegression::fit, "Train the model",
+//              py::arg("X_train"), py::arg("y_train"))
+//         .def("predict", &LinearRegression::predict, "Make predictions",
+//              py::arg("X_test"));
+
+//     py::class_<NormalEquation, LinearRegression, std::shared_ptr<NormalEquation>>(m, "NormalEquation")
+//         .def(py::init<>())
+//         .def("fit", &NormalEquation::fit, "Train the model",
+//             py::arg("X_train"), py::arg("y_train"))
+//         .def("predict", &NormalEquation::predict, "Make predictions",
+//              py::arg("X_test"));
+// }
+
 PYBIND11_MODULE(linregpy, m) {
-    m.doc() = "Python bindings for the C++ LinearRegression library";
-    py::class_<LinearRegression>(m, "LinearRegression")
-        .def("fit", &LinearRegression::fit, "Train the model",
-             py::arg("X_train"), py::arg("y_train"))
-        .def("predict", &LinearRegression::predict, "Make predictions",
-             py::arg("X_test"));
+    py::class_<LinearRegression, std::shared_ptr<LinearRegression>>(m, "LinearRegression")
+        .def("fit", &LinearRegression::fit)
+        .def("predict", &LinearRegression::predict);
 
     py::class_<NormalEquation, LinearRegression, std::shared_ptr<NormalEquation>>(m, "NormalEquation")
-        .def(py::init<>())
-        .def("fit", &NormalEquation::fit, "Train the model",
-            py::arg("X_train"), py::arg("y_train"))
-        .def("predict", &NormalEquation::predict, "Make predictions",
-             py::arg("X_test"));
+        .def(py::init<>());
+
 }
